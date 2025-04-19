b9fa69e6daf6f778fa90acc9b71573841caab945
chousik <Zaxap2018@yandex.ru>
2025-04-19 05:32:44 +0300

    add jwt to getaway

--- gateway-server/build.gradle.kts
+++ gateway-server/build.gradle.kts
@@ -21,12 +21,13 @@ extra["springCloudVersion"] = "2024.0.1"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+//	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 dependencyManagement {
