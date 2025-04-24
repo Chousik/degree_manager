0eadca2e370c7dcb4ff2f83537392ddbb8b707a6
chousik <Zaxap2018@yandex.ru>
2025-04-24 22:35:17 +0300

    add stucture#2

--- auth-service/src/main/java/ru/chousik/web/authservice/entity/TeacherEntity.java
+++ auth-service/src/main/java/ru/chousik/web/authservice/entity/TeacherEntity.java
@@ -1,50 +1,22 @@
 package ru.chousik.web.authservice.entity;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.util.UUID;
 
 @Entity
-@Table(name = "teachers")
+@Table("teachers")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@JsonTypeName("ru.chousik.web.authservice.entity.TeacherEntity")
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "@class"
-)
 public class TeacherEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     UUID id;
-
-    @Size(max = 50)
-    @NotNull
-    String name;
-
-    @Size(max = 50)
-    @NotNull
-    String surname;
-
-    @Column(name = "middle_name")
-    @Size(max = 50)
-    @NotNull
-    String middleName;
-
-    public TeacherEntity(String name,
-                         String surname,
-                         String middleName){
-        this.name = name;
-        this.surname = surname;
-        this.middleName = middleName;
-    }
+    String username;
+    String password;
 }
--- auth-service/src/main/java/ru/chousik/web/authservice/entity/UserEntity.java
+++ auth-service/src/main/java/ru/chousik/web/authservice/entity/UserEntity.java
@@ -1,36 +1,16 @@
 package ru.chousik.web.authservice.entity;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Entity
-@Table(name = "users")
+@Table("users")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@JsonTypeName("ru.chousik.web.authservice.entity.UserEntity")
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "@class"
-)
 public class UserEntity {
-    @Id
-    String username;
-
-    @NotNull
-    String password;
-
-    @NotNull
-    Boolean enabled;
-
-    @OneToOne
-    @JoinColumn(name = "teacher_id", unique = true)
-    TeacherEntity teacher;
 }
