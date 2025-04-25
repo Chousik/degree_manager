e0dd7c4e68c8648c516badd2c34b9b2477a7df66
hipeoplea <hipeoplea@yandex.ru>
2025-04-25 11:50:52 +0300

    2users

--- auth-service/src/main/java/ru/chousik/web/authservice/config/ToolsConfig.java
+++ auth-service/src/main/java/ru/chousik/web/authservice/config/ToolsConfig.java
@@ -4,21 +4,34 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import ru.chousik.web.authservice.repository.AuthoritiesRepository;
-import ru.chousik.web.authservice.repository.UserRepository;
-import ru.chousik.web.authservice.security.DegreeUserDetailServices;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ToolsConfig {
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository,
-                                                 AuthoritiesRepository authoritiesRepository){
-        return new DegreeUserDetailServices(authoritiesRepository, userRepository);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        if (!jdbcUserDetailsManager.userExists("chousik")) {
+            jdbcUserDetailsManager.createUser(User.withUsername("chousik")
+                    .password(passwordEncoder().encode("chousik"))
+                    .roles("ADMIN")
+                    .build());
+        }
+        if (!jdbcUserDetailsManager.userExists("hipeoplea")) {
+            jdbcUserDetailsManager.createUser(User.withUsername("hipeoplea")
+                    .password(passwordEncoder().encode("hipeoplea"))
+                    .roles("USER")
+                    .build());
+        }
+        return jdbcUserDetailsManager;
     }
 
     @Bean
