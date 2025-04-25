fd817fc511c11aeaaafba92b7ffdcddacaf2f356
chousik <Zaxap2018@yandex.ru>
2025-04-25 12:23:10 +0300

    add students&teacher controller

---
+++ task-service/src/main/java/ru/chousik/web/taskservice/controller/StudentController.java
@@ -0,0 +1,25 @@
+package ru.chousik.web.taskservice.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.chousik.web.taskservice.entity.StudentEntity;
+import ru.chousik.web.taskservice.repository.StudentRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teacher")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class StudentController {
+    StudentRepository studentRepository;
+
+    @GetMapping
+    List<StudentEntity> getStudents(){
+        return studentRepository.findAll();
+    }
+}
