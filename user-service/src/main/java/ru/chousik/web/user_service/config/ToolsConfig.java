5a4583df5670a4b31432090ca86cde119ae8a873
Zahar Silaev <78282521+Chousik@users.noreply.github.com>
2025-06-04 17:14:41 +0300

    chore: remove unused logging filter import

--- user-service/src/main/java/ru/chousik/web/user_service/config/ToolsConfig.java
+++ user-service/src/main/java/ru/chousik/web/user_service/config/ToolsConfig.java
@@ -3,7 +3,6 @@ package ru.chousik.web.user_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class ToolsConfig {
