9d403a891df4abd2fc04d043842513a008483a1c
chousik <Zaxap2018@yandex.ru>
2025-12-22 01:15:24 +0300

    HARDCORE

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-core:10.20.2")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	implementation("org.modelmapper:modelmapper:3.1.1")
